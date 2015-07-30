ce156390dd9e1f314acbfbfffd5f7da7c7397633
Sash Zats <sash@zats.io>
2015-07-29 21:42:10 -0700

    Added MATT Helper

---
+++ MATT Helper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MATT Helper
+//
+//  Created by Sash Zats on 7/29/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MATT Helper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MATT Helper
+//
+//  Created by Sash Zats on 7/29/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
