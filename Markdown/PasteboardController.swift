95a10401dba78cb165035e87a67b13c4c6c7b02f
Sash Zats <sash@zats.io>
2015-08-03 21:03:50 -0700

    Added pasteboard controller

---
+++ Markdown/PasteboardController.swift
@@ -0,0 +1,38 @@
+//
+//  PasteboardController.swift
+//  Markdown
+//
+//  Created by Sash Zats on 8/3/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import AppKit
+
+
+struct PasteboardController {
+    
+    static func pasteboardContents() -> [NSPasteboardItem]? {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        return pasteboard.pasteboardItems as? [NSPasteboardItem]
+    }
+    
+    static func setPasteboardContents(items: [NSPasteboardItem]) {
+        let pasteboard = NSPasteboard.generalPasteboard()
+        pasteboard.clearContents()
+        pasteboard.writeObjects(items)
+    }
+}
+
+extension NSPasteboardItem: NSCopying {
+    public func copyWithZone(zone: NSZone) -> AnyObject {
+        let item = NSPasteboardItem()
+        if let types = self.types {
+            for type in types as! [String] {
+                if let data = self.dataForType(type) {
+                    item.setData(data, forType: type)
+                }
+            }
+        }
+        return item
+    }
+}
