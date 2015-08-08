ef842e1ecc842bb2bb9b1053760d3f827741a10c
Sash Zats <sash@zats.io>
2015-08-08 14:44:25 -0700

    first time controller struct → class

--- Markdown/FirstTimeController.swift
+++ Markdown/FirstTimeController.swift
@@ -9,41 +9,53 @@
 import AppKit
 
 
-struct FirstTimeController {
+class FirstTimeController {
     
-    static func executeIfNeeded(window: NSWindow, completion: Void -> Void) {
+    private static let preferncesController = PreferencesController()
+    
+    static func executeIfNeeded(presenter: NSViewController, completion: Void -> Void) {
         if !shouldShowFirstTimeExperience() {
             return
         }
-        execute(window, completion: completion)
+        execute(presenter, completion: completion)
+    }
+    
+    static func finish() {
+        if let currentVersion = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String {
+            preferncesController.lastAppVersion = currentVersion
+        } else {
+            assertionFailure("Can't extract CFBundleShortVersionString from Info.plist \(NSBundle.mainBundle().infoDictionary)")
+        }
     }
     
     // MARK: - Private
     
-    private static func execute(window: NSWindow, completion: Void -> Void) {
-        let alert = NSAlert()
-        alert.messageText = "\(AppManager.shortName) is more fun when you don't need to remember to launch it."
-        alert.informativeText = "Do you want to enable \(AppManager.shortName) to run automatically upon startup?"
-        alert.addButtonWithTitle("Enable")
-        alert.addButtonWithTitle("Cancel")
-        alert.beginSheetModalForWindow(window) { results in
-            if results == NSAlertFirstButtonReturn {
-                // Enable
-                println("Enable login item")
-            }
-            completion()
+    private static func execute(presenter: NSViewController, completion: Void -> Void) {
+        if let storyboard = NSStoryboard(name: "Main", bundle: nil),
+            controller = storyboard.instantiateControllerWithIdentifier("Intro") as? IntroViewController {
+                presenter.presentViewControllerAsSheet(controller)
         }
+//        let alert = NSAlert()
+//        alert.messageText = "\(AppManager.shortName) is more fun when you don't need to remember to launch it."
+//        alert.informativeText = "Do you want to enable \(AppManager.shortName) to run automatically upon startup?"
+//        alert.addButtonWithTitle("Enable")
+//        alert.addButtonWithTitle("Cancel")
+//        alert.beginSheetModalForWindow(window) { results in
+//            if results == NSAlertFirstButtonReturn {
+//                // Enable
+//                println("Enable login item")
+//            }
+//            completion()
+//        }
     }
     
     private static func shouldShowFirstTimeExperience() -> Bool {
         if let currentVersion = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String {
-            var preferncesController = PreferencesController()
             if let lastVersion = preferncesController.lastAppVersion {
                 if lastVersion == currentVersion {
                     return false
                 }
             }
-            preferncesController.lastAppVersion = currentVersion
             return true
         } else {
             assertionFailure("Can't extract CFBundleShortVersionString from Info.plist \(NSBundle.mainBundle().infoDictionary)")
