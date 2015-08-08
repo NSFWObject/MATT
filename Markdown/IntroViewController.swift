3e9c6b55e25be64bd9fd9fd8303b53c14e966513
Sash Zats <sash@zats.io>
2015-08-08 14:43:54 -0700

    intro view controller

---
+++ Markdown/IntroViewController.swift
@@ -0,0 +1,26 @@
+//
+//  IntroViewController.swift
+//  Markdown
+//
+//  Created by Sash Zats on 8/7/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import AppKit
+
+
+class IntroViewController: NSViewController {
+    @IBOutlet weak var installAppleScriptsCheckbox: NSButton!
+    @IBOutlet weak var launchOnLoginCheckbox: NSButton!
+    
+    var scriptController: ScriptManager!
+    
+    @IBAction func _doneButtonAction(sender: AnyObject) {
+        presentingViewController?.dismissViewController(self)
+        if installAppleScriptsCheckbox.state == NSOnState {
+            scriptController.installScripts{ _ in }
+        }
+    }
+    
+    // MARK: - Private
+}
