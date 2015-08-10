169c18909d51457d0e322e9763a6bf86942784c1
Sash Zats <sash@zats.io>
2015-08-10 08:00:56 -0700

    extracted Script into a separate file

---
+++ Markdown/Script.swift
@@ -0,0 +1,58 @@
+//
+//  Script.swift
+//  Markdown
+//
+//  Created by Sash Zats on 8/10/15.
+//  Copyright (c) 2015 Sash Zats. All rights reserved.
+//
+
+import Foundation
+
+
+public enum Script {
+    case Copy
+    case Paste
+}
+
+extension Script {
+    var fileName: String {
+        switch self {
+        case .Copy:
+            return "⌘C"
+        case .Paste:
+            return "⌘V"
+        }
+    }
+    
+    var bundledURL: NSURL {
+        return NSBundle.mainBundle().URLForResource(self.fileName, withExtension: "scpt", subdirectory: "Scripts")!
+    }
+    
+    var destinationURL: NSURL? {
+        let manager = NSFileManager.defaultManager()
+        if let URL = manager.URLForDirectory(.ApplicationScriptsDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: false, error: nil) {
+            return URL.URLByAppendingPathComponent("\(self.fileName).scpt")
+        }
+        return nil
+    }
+}
+
+public extension Script {
+    func execute(handler: Bool -> Void) {
+        if let URL = self.destinationURL {
+            var error: NSError?
+            if let task = NSUserAppleScriptTask(URL: URL, error: &error) {
+                task.executeWithCompletionHandler{ error in
+                    assert(error == nil, "Failed to execute script \(error)")
+                    handler(error == nil)
+                }
+            } else {
+                assertionFailure("Failed to create task: \(error)")
+                handler(false)
+            }
+        } else {
+            assertionFailure("Script \(self) is not found in the destination folder")
+            handler(false)
+        }
+    }
+}
