c5d3e51a2054ec4d8e03bbbb6cdacbff8883353d
Sash Zats <sash@zats.io>
2015-08-12 09:59:23 -0700

    process markdown in place

--- Markdown/AppController.swift
+++ Markdown/AppController.swift
@@ -30,8 +30,7 @@ public class AppController {
     public func process(#markdown: String, completion: Result<ProcessingResult, ProcessingError> -> Void) {
         // preserve pasteboard
         let pasteboardState = pasteboardController.pasteboardState()
-
-        // set pasteboard to the processed markdown
+        
         let styleContents = styleManager.content(style: styleManager.selectedStyle)
         let renderResult = renderEngine.render(markdown: markdown, style: styleContents)
         pasteboardController.writeToPasteboard{ pasteboard in
@@ -45,21 +44,60 @@ public class AppController {
             completion(.success(.CopiedToPasteboard))
             return
         }
-
+        
         focusController.switchToCapturedApp{ result in
             switch result {
             case .Success:
                 Script.Paste.execute{ success in
                     assert(success, "Failed to paste!")
                     self.pasteboardController.restorePasteboardState(pasteboardState)
-                    if success {
-                        completion(success ? .success(.PastedToCapturedApp) : .failure(.ScriptError))
-                    }
+                    completion(success ? .success(.PastedToCapturedApp) : .failure(.ScriptError))
                 }
             case .Failure(let error):
                 completion(.failure(.AppFocusError(error.value)))
             }
         }
-   }
+    }
+    
+    public func processInPlace(completion: Bool -> Void) {
+        
+        while NSEvent.modifierFlags() != NSEventModifierFlags.allZeros {
+            // no-op
+        }
+        
+        if let app = focusController.activeApp() {
+            if app.bundleIdentifier == AppIdentity.bundleId {
+                // disable in our app
+                completion(false)
+                return
+            }
+            let pasteboardState = pasteboardController.pasteboardState()
+            Script.Copy.execute{ result in
+                assert(result, "Failed to copy")
+                if let markdown = self.pasteboardController.stringContent() {
+                    println("Buffer content: \(markdown)")
+                    let styleContents = self.styleManager.content(style: self.styleManager.selectedStyle)
+                    let renderResult = self.renderEngine.render(markdown: markdown, style: styleContents)
+                    println("renderResult \(renderResult.HTMLString)")
+                    self.pasteboardController.writeToPasteboard{ pasteboard in
+                        pasteboard.writeObjects([renderResult.attributedString])
+                        // for apps that understand in `div`s and `span`s (Evernote, Safari etc… Not Pages:/)
+                        pasteboard.setString(renderResult.HTMLString, forType: NSPasteboardTypeHTML)
+                    }
+                    Script.Paste.execute{ success in
+                        assert(success, "Failed to paste!")
+                        self.pasteboardController.restorePasteboardState(pasteboardState)
+                        completion(success)
+                    }
+                } else {
+                    self.pasteboardController.restorePasteboardState(pasteboardState)
+                }
+            }
+        } else {
+            assertionFailure("No active application")
+            completion(false)
+        }
+    }
+    
     
 }
